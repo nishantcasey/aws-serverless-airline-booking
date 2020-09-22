e03a6a8be576c5cba95b4579e3644e567c88c6d2
Nishant Casey <cnishant@amazon.com>
2020-09-22 13:30:12 +0100

    Modifiying dietary requirements

--- src/frontend/views/Profile.vue
+++ src/frontend/views/Profile.vue
@@ -133,6 +133,8 @@ export default {
             { label: "Vegatarian", value: "vegetarian", color: "secondary" },
             { label: "Vegan", value: "vegan" },
             { label: "Dairy-free", value: "dairy-free" },
+            { label: "Kosher", value: "kosher" },
+            { label: "Pizza-only", value: "pizza" },
             { label: "Regular", value: "regular" }
           ]
         }
